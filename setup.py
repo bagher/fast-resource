8fdac165aab3b490f8d55c0cb1b182c484bb0c2e
rokni <bagher.rokni@gmail.com>
2023-02-07 13:08:04 +0330

    fix: Github url

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
     version='0.1.0',
     author='Bagher Rokni',
     author_email='bagher.rokni@gmail.com',
-    packages=find_packages(),
-    url='http://pypi.python.org/pypi/fast-resource/',
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    url='https://github.com/bagher/fast-resource',
     license='LICENSE.txt',
-    description='fast-resource is a data transformation layer between the database data and data returned to the '
+    description='fast-resource is a data transformation layer between the database and data returned to the '
                 'application\'s users. Also, it can cache data by using Redis and Memcached.',
     long_description=open('README.md').read(),
     install_requires=['redis', 'pymemcache'],
